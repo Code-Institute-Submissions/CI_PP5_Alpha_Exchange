12866b8452019b4700f5096a09f816e715da742a
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-10-17 19:45:35 +0000

    wired up product views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
+"""
+A module containing the views within products app.
+"""
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def list_products(request):
+    """
+    Displays the list of products with filtering and search queries.
+    """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+        }
+
+    return render(request, 'products/products.html', context)
