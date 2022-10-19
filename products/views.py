6e2dbc7ba4b710df3b9e703ba2b2dc85b45ad590
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-10-19 14:02:27 +0000

    added products detail page

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 """
 A module containing the views within products app.
 """
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage
 from .models import Product
 
@@ -20,3 +20,17 @@ def list_products(request, page=1):
         products = paginator.page(paginator.num_pages)
 
     return render(request, 'products/products.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    """
+    Displays the product details page by product ID.
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'products/product_detail.html', context)
