4ebbb560891b93c1f3b69f7e20427487134489cc
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-10-27 17:41:46 +0000

    added add_to_basket function and links, re-made buttons as forms

--- basket/views.py
+++ basket/views.py
@@ -1,11 +1,36 @@
 """
 A module containing the views within basket app.
 """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from products.models import Product
 
 
 def basket(request):
     """
-    A view tp display the current basket.
+    A view to display the current basket.
     """
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """
+    Add the given quantity of the item to the basket.
+    """
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+        messages.success(
+            request, f'Added {Product.name} to your basket!'
+            )
+    else:
+        basket[item_id] = quantity
+        messages.success(
+            request, f'Added {Product.name} to your basket!'
+            )
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
