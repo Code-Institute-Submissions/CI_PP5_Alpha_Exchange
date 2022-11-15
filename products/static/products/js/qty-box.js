fefeef6260725d9ee1a9fd2f689e96f6c22bea6f
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-11-15 13:08:39 +0000

    update qty-box.js and quantity selectors on all pages to avoid duplicate ID's

--- products/static/products/js/qty-box.js
+++ products/static/products/js/qty-box.js
@@ -1,45 +1,72 @@
 // Limits the range of the buttons to 1-99
-function disableOption(itemId) {
-    var quantity = parseInt($(`#id_qty_${itemId}`).val());
+
+function disableOption(itemId, size) {
+    if (size) {
+        var quantity = parseInt($(`.size_${itemId}_${size}`).val());
+    } else {
+        var quantity = parseInt($(`.id_qty_${itemId}`).val());
+    }
+
     var minusDisabled = quantity < 2;
     var plusDisabled = quantity > 98;
-    $(`#decrement-qty_${itemId}`).prop('disabled', minusDisabled);
-    $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled);
+
+    if (size) {
+        $(`.decrement-size_${itemId}_${size}`).prop('disabled', minusDisabled);
+        $(`.increment-size_${itemId}_${size}`).prop('disabled', plusDisabled);
+    } else {
+        $(`.decrement-qty_${itemId}`).prop('disabled', minusDisabled);
+        $(`.increment-qty_${itemId}`).prop('disabled', plusDisabled);
+    }
 }
 
 // disables buttons correctly on page load
 var allQtyInputs = $('.select_input');
 for(var i = 0; i < allQtyInputs.length; i++){
     var itemId = $(allQtyInputs[i]).data('item_id');
-    disableOption(itemId);
+    var size = $(allQtyInputs[i]).data('size');
+    disableOption(itemId, size);
+    
 }
 
 // checks to disable function on each click
 $('.select_input').change(function() {
     var itemId = $(this).data('item_id');
-    disableOption(itemId);
+    var size = $(this).data('size');
+    disableOption(itemId, size);
 });
 
 // Increment product quantity
 $('.increment-qty').click(function(e) {
    e.preventDefault();
+   var itemId = $(this).data('item_id');
+   var size = $(this).data('size');
    // get the parent input group then search for select input class
    var parent = $(this).closest('.input-group').find('.form-control')[0];
+   if (size) {
+        var allQuantityInputs = $(`.input-group-${itemId} input[data-size='${size}']`);
+    } else {
+        var allQuantityInputs = $(`.input-group-${itemId} input[name='quantity']`);
+    }
    var quantity = parseInt($(parent).val());
    // set new quantity box value
-   $(parent).val(quantity + 1);
-   var itemId = $(this).data('item_id');
-   disableOption(itemId);
+   $(allQuantityInputs).val(quantity + 1);
+   disableOption(itemId, size);
 });
 
 // Decrement product quantity
 $('.decrement-qty').click(function(e) {
    e.preventDefault();
+   var itemId = $(this).data('item_id');
+   var size = $(this).data('size');
    // get the parent input group then search for select input class
    var parent = $(this).closest('.input-group').find('.form-control')[0];
+   if (size) {
+        var allQuantityInputs = $(`.input-group-${itemId} input[data-size='${size}']`);
+    } else {
+        var allQuantityInputs = $(`.input-group-${itemId} input[name='quantity']`);
+    }
    var quantity = parseInt($(parent).val());
    // set new quantity box value
-   $(parent).val(quantity - 1);
-   var itemId = $(this).data('item_id');
-   disableOption(itemId);
+   $(allQuantityInputs).val(quantity - 1);
+   disableOption(itemId, size);
 });
